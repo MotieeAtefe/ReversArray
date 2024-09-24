020f3e165b5faa718fd23e61526a814546918933
motie <motie@DESKTOP-8QOGGKP>
2024-09-24 16:16:50 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace reversArray
+{
+    internal class Program
+    {
+        int[]orginalArray;
+        static void Main(string[] args)
+        {
+            
+        }
+        private void ReverseArray()
+        {
+            int start = 0;
+            int temp = 0;
+            int end = orginalArray.Length;
+            while(start < end)
+            {
+                temp = orginalArray[start];
+                orginalArray[start] = orginalArray[end];
+                orginalArray[end] = temp;
+                start++;
+                end--;
+            }
+
+        }
+        private void PrintArray()
+        {
+            foreach(int value in orginalArray)
+            {
+                Console.Write($"{value }");
+            }
+        }
+    }
+}
